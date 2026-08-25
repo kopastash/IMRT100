1790a3bc94f11b662250575d173b47d37bed458d
Henrik <henrikjlarsson@hotmail.com>
2026-08-25 13:02:13 +0200

    Changed motor logic.

--- python/raspi/imrt_robot_sensor_custom.py
+++ python/raspi/imrt_robot_sensor_custom.py
@@ -116,6 +116,7 @@ while not motor_serial.shutdown_now :
 
     if frontsensor < 10 & dist_5 < 20:
         roter(VENSTRE, 0.5)
+        print("Roterer")
     else:
         motor_serial.send_command(int(MODFART + (MODFART * normalized * 0.8)), int(MODFART - (MODFART * normalized * 0.8)))
 
